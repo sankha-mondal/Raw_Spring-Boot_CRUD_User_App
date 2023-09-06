c0bd83da174adfd4229ecfd41f7b18fef60f97ac
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2023-09-06 21:05:11 +0530

    Raw Spring-Boot User Application

--- Raw_SB_User_App/src/main/java/com/repository/User_Repository.java
+++ Raw_SB_User_App/src/main/java/com/repository/User_Repository.java
@@ -9,11 +9,9 @@ import com.entity.User;
 
 @Repository
 public interface User_Repository extends JpaRepository<User, String> {
-	
-	//  Retrieve data by Email & Password Operation:-  Op: 5
-	
-//	@Query("select p from Passenger p where p.pEmail = :pEmail and p.pPassword = :pPassword")
-//	public int findPassengerByEmail_Pass(@Param("pEmail") String pEmail,@Param("pPassword") String pPassword);
+
+	public User findUserByuNameIgnoreCase(String uName);  // Op: 2B
+	public List<User> findUserByuGenderIgnoreCase(String uGender);  // Op: 2C
 
 }
 
